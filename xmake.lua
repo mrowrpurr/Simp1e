59da6b1ac292b5dac074f929b1a7563ffe6aed42
Mrowr Purr <mrowr.purr@gmail.com>
2023-05-17 14:33:28 -0700

    Adding Specs

--- xmake.lua
+++ xmake.lua
@@ -3,13 +3,17 @@ add_rules("mode.debug", "mode.release")
 set_languages("cxx20")
 add_toolchains("msvc")
 
-local gooey_path = os.getenv("GOOEY_PATH")
-if gooey_path then
-    includes(path.join(gooey_path, "Gooey"))
-    includes(path.join(gooey_path, "Gooey.wxWidgets"))
-else
-    print("GOOEY_PATH not set")
-    return
-end
+add_repositories("MrowrLib https://github.com/MrowrLib/Packages.git")
+add_requires("string_format", "_Log_", "spdlog")
+
+-- -- Include the Gooey library for UI
+-- local gooey_path = os.getenv("GOOEY_PATH")
+-- if gooey_path then
+--     includes(path.join(gooey_path, "Gooey"))
+--     includes(path.join(gooey_path, "Gooey.FLTK"))
+-- else
+--     print("GOOEY_PATH not set")
+--     return
+-- end
 
 includes("**/xmake.lua")
