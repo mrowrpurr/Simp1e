e164771104fbf7cdb32c57baae70a3c4c8d6ba3d
Mrowr Purr <mrowr.purr@gmail.com>
2023-05-20 20:31:20 -0700

    add_packages("string_format")

--- xmake/string_format.lua
+++ xmake/string_format.lua
@@ -8,6 +8,7 @@ function add_requires_for_string_formatting()
 end
 
 function add_string_formatting()
+    add_packages("string_format", {public = true})
     if not is_host("windows") then
         add_packages("fmt", {public = true}) -- MSVC doesn't need this (supports std::format)
         add_defines("STRING_FORMAT_USE_FMT") -- Mac Clang needs this (at the moment)
