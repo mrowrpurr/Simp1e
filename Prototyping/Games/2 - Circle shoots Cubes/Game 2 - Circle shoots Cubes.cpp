864b5061ba5b8c6c1cffb8e7635704df356fe50e
Mrowr Purr <mrowr.purr@gmail.com>
2023-05-25 10:32:54 -0700

    Fresh folder

--- Prototyping/Games/2 - Circle shoots Cubes/Game 2 - Circle shoots Cubes.cpp
+++ Prototyping/Games/2 - Circle shoots Cubes/Game 2 - Circle shoots Cubes.cpp
@@ -1,19 +1,10 @@
 #include <QApplication>
-
-#include "Prototyping/GameGraphicsView.h"
-#include "Prototyping/GameMapForCircleAndCubes.h"
+#include <QWidget>
 
 int main(int argc, char* argv[]) {
-    QApplication                          app{argc, argv};
-    Prototyping::GameMapForCircleAndCubes map{10, 20};
-    Prototyping::GameGraphicsView         view;
-    view.SetScene(map.GetScene());
-    // Move the view handling into the game
-    view.OnUpKey([&map]() { map.MoveCircleUp(); });
-    view.OnDownKey([&map]() { map.MoveCircleDown(); });
-    view.OnLeftKey([&map]() { map.MoveCircleLeft(); });
-    view.OnRightKey([&map]() { map.MoveCircleRight(); });
-    view.setFixedSize(map.GetWidth() + 10, map.GetHeight() + 10);
-    view.show();
+    QApplication app{argc, argv};
+    QWidget      widget;
+    widget.setFixedSize(100, 100);
+    widget.show();
     return app.exec();
 }
