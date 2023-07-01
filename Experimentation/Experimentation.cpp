c1386a37de3421cde91ee42ea18d62d29ed62ce9
Mrowr Purr <mrowr.purr@gmail.com>
2023-06-30 22:42:00 -0700

    MemberFunctionPointer uses ValueWrapper too

--- Experimentation/Experimentation.cpp
+++ Experimentation/Experimentation.cpp
@@ -15,10 +15,10 @@ std::vector<std::unique_ptr<IFunctionPointer>> _callbacks;
 
 void RegisterCallback(IFunctionPointer* ptr) { _callbacks.push_back(std::unique_ptr<IFunctionPointer>(ptr)); }
 
-// template <typename T>
-// void RegisterCallback(T* ptr, int (T::*func)(int)) {
-//     RegisterCallback(new MemberFunctionPointer<T, void, int>(ptr, func));
-// }
+template <typename T>
+void RegisterCallback(T* ptr, int (T::*func)(int)) {
+    RegisterCallback(new MemberFunctionPointer<T, int, int>(func, ptr));
+}
 
 void RegisterCallback(int (*func)(int)) { RegisterCallback(new FunctionPointer<int, int>(func)); }
 
@@ -27,24 +27,26 @@ int ThisIsAFuntion(int value) {
     return value * 2;
 }
 
+struct ThisIsAStruct {
+    int ThisIsAMemberFunction(int value) {
+        _Log_("ThisIsAMemberFunction called with value = {}", value);
+        return value * 3;
+    }
+};
+
 void RunCallbacks() {
-    // for (auto& callback : _callbacks) callback->Invoke(42);
     for (auto& callback : _callbacks) {
-        if (auto* functionPointer = dynamic_cast<FunctionPointer<int, int>*>(callback.get())) {
-            IValueWrapper* args[1];
-            args[0]     = new ValueWrapper<int>(69);
-            auto result = static_cast<ValueWrapper<int>*>(functionPointer->Invoke(args))->GetValue();
-            _Log_("result = {}", result);
-        }
-        // } else if (auto* memberFunctionPointer = dynamic_cast<MemberFunctionPointer<Environment, void,
-        // int>*>(callback.get())) {
-        //     memberFunctionPointer->Invoke(nullptr, 42);
-        // }
+        IValueWrapper* args[1];
+        args[0]     = new ValueWrapper<int>(69);
+        auto result = static_cast<ValueWrapper<int>*>(callback->Invoke(args))->GetValue();
+        _Log_("result = {}", result);
     }
 }
 
 int main() {
     _Log_("hi");
     RegisterCallback(ThisIsAFuntion);
+    ThisIsAStruct thisIsAStruct;
+    RegisterCallback(&thisIsAStruct, &ThisIsAStruct::ThisIsAMemberFunction);
     RunCallbacks();
 }
--- Simp1e.Core/include/Simp1e/MemberFunctionPointer.h
+++ Simp1e.Core/include/Simp1e/MemberFunctionPointer.h
@@ -3,6 +3,7 @@
 #include <utility>
 
 #include "IFunctionPointer.h"
+#include "ValueWrapper.h"
 
 namespace Simp1e {
 
@@ -11,12 +12,21 @@ namespace Simp1e {
         ReturnType (T::*_func)(Args...);
         T* _instance;
 
+        template <std::size_t... I>
+        IValueWrapper* InvokeImpl(std::index_sequence<I...>, IValueWrapper** args) {
+            return new ValueWrapper<ReturnType>((_instance->*_func)(
+                static_cast<ValueWrapper<typename std::tuple_element<I, std::tuple<Args...>>::type>*>(args[I])
+                    ->GetValue()...
+            ));
+        }
+
     public:
         MemberFunctionPointer(ReturnType (T::*func)(Args...), T* instance) : _func(func), _instance(instance) {}
 
         bool IsMemberFunction() const override { return true; }
 
-        ReturnType Invoke(Args... args) { return (_instance->*_func)(std::forward<Args>(args)...); }
-        ReturnType operator()(Args... args) { return Invoke(std::forward<Args>(args)...); }
+        IValueWrapper* Invoke(IValueWrapper** args) override {
+            return InvokeImpl(std::index_sequence_for<Args...>{}, args);
+        }
     };
 }
